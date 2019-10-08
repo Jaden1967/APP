bc92a3bba66a6ef6edcaf740c9fb7f166510913a
AllanInComp <35985623+AllanInComp@users.noreply.github.com>
2019-10-07 20:22:43 -0400

    Map loader adapt
    Entities adapts map loader features
    Continent and Country have proper constructors to allow initialization via map loading from file

--- src/Entities/Country.java
+++ src/Entities/Country.java
@@ -9,13 +9,47 @@ public class Country {
 		private Vector <Country> linkCountries;
 		private int armyNum = 0;
 		private Continent belongTo;
+		private int x;
+		private int y;
 		
-		public Country() {
-			
+		
+		public Country() {}
+		
+		
+		/**
+		 * Constructor for Country
+		 * @param id serialized String for country
+		 * @param name proper name for Country
+		 * @param horizontal location on x axis on map
+		 * @param vertical location on y axis on map
+		 */
+		public Country(int id, String name, int horizontal, int vertical) {
+			this.countryId = id;
+			this.countryName = name;
+			this.x = horizontal;
+			this.y = vertical;
+			this.linkCountries = new Vector<>();
+		}
+		
+		public void AssignOwner (Player p) {
+			this.owner = p;
 		}
 		
 		public Player getOwner() {
 			return this.owner;
+		}
+		
+		/**
+		 * Adds Country object in argument to linkedCountries Vector as a neighbor
+		 * does't need to add this to neighbour's linkedCountries the same function will be called for them too
+		 * @param nbour
+		 */
+		public void addNeighbour(Country nbour) {
+			for (Country c: this.linkCountries) {
+				if (c.getID() == nbour.getID())
+					return;
+			}
+			this.linkCountries.add(nbour);
 		}
 		
 		/**
@@ -36,13 +70,21 @@ public class Country {
 		
 		/**
 		 * During end of attack in phases 2 and fortification in phase 3
+		 * First verifies if country is connected and the army number to be moved is valid
+		 * if verification is passed, adjusts army number of both countries accordingly
 		 * @param toID id of connected country
 		 * @param armyNum amount of army to move to destination
 		 * @return whether or not moving is successful
 		 */
 		public boolean moveArmy(int toID, int armyNum) {
 			if (this.armyNum < armyNum+1) return false;
-			
+			boolean neighBourExists = false;
+			for(Country c: this.linkCountries) {
+				if (c.getID() == toID) {
+					neighBourExists = true;
+				}
+			}
+			if(!neighBourExists) return false;
 			Country temp = new Country();
 			for (Country c: linkCountries) {
 				temp = c;
