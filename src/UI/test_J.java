d833b9ea37cb975061d33c985842390a7dca25f7
KingDeath233 <35985749+KingDeath233@users.noreply.github.com>
2019-10-12 20:25:59 -0400

    visualization
    visualization

---
+++ src/UI/test_J.java
@@ -0,0 +1,69 @@
+package UI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+
+import UI.Game.mapPanel;
+
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+
+public class test_J extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					test_J frame = new test_J();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	class mapPanel extends JPanel{
+		public void paintComponent(Graphics g) {
+			Image i = new ImageIcon("Image\\luca_pic.jpg").getImage();
+			Dimension size = this.getParent().getSize();
+			g.drawImage(i,0,0,940,585,this);
+		}
+	}
+	
+	/**
+	 * Create the frame.
+	 */
+	public test_J() {
+		setTitle("Risk");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1000, 750);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(null);
+		setContentPane(contentPane);
+		
+		mapPanel map = new mapPanel();
+		map.setBounds(20, 20, 940, 585);
+		map.setLayout(null);
+		contentPane.add(map);
+	}
+}
